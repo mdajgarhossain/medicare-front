b33b6aa874a3a1ace09dda19c485265f6a30a04f
Md. Ajgar Hossain (Jewell) <mdajgarhossain1@gmail.com>
2023-08-27 00:21:59 +0600

    Created SocialMediaIcons component

---
+++ src/components/common/SocialMediaIcons.jsx
@@ -0,0 +1,59 @@
+import { FaFacebookF, FaTwitter, FaYoutube } from "react-icons/fa";
+import { TfiLinkedin } from "react-icons/Tfi";
+
+const SocialMediaIcons = () => {
+  return (
+    <div className="justify-end gap-2 mt-2 lg:flex">
+      <a
+        href="https://www.facebook.com/profile.php?id=100063579536034"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex rounded-lg items-center lg:px-3 py-2 px-2 my-[2px] lg:my-0 text-white bg-[#1877f2]"
+      >
+        <span className="sr-only">Facebook</span>
+        <FaFacebookF className="w-3 h-3 lg:h-5 lg:w-5" />
+      </a>
+
+      <a
+        href="https://twitter.com/Mohamma85629351"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex rounded-lg items-center lg:px-3 py-2 px-2 my-[2px] lg:my-0 text-white bg-[#1da1f3]"
+      >
+        <FaTwitter className="w-3 h-3 text-white lg:h-5 lg:w-5" />
+      </a>
+
+      <a
+        href="https://www.instagram.com/hasaninternational2020/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex items-center text-white rounded-lg"
+      >
+        <img
+          className="lg:h-10 lg:w-10 w-[28px] h-6 lg:my-0 my-[2px]"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/132px-Instagram_logo_2016.svg.png?20210403190622"
+          alt="Instagram"
+        />
+      </a>
+
+      <a
+        href="https://www.linkedin.com/signup/cold-join?session_redirect=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2Ffeed%2F%3Ftrk%3Donboarding-landing&trk=login_reg_redirect"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex rounded-lg items-center lg:px-3 py-2 px-2 my-[2px] lg:my-0 text-white bg-[#0077b5]"
+      >
+        <TfiLinkedin className="w-3 h-3 lg:h-5 lg:w-5" />
+      </a>
+      <a
+        href="https://www.youtube.com/channel/UCrAQh80mx-kM5Z7lE52mJLw"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex rounded-lg items-center lg:px-3 py-2 px-2 my-[2px] lg:my-0 text-white bg-[#ff0000]"
+      >
+        <FaYoutube className="w-3 h-3 text-white lg:h-5 lg:w-5" />
+      </a>
+    </div>
+  );
+};
+
+export default SocialMediaIcons;
