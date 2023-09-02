33852aa14521fe7269d4f8b02293c4505b6c5f3c
Md. Ajgar Hossain (Jewell) <mdajgarhossain1@gmail.com>
2023-09-02 19:48:28 +0600

    Wrap _app.js with conditional routing & RoutingComponent

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,38 @@
 import '../styles/globals.css';
+import { useRouter } from "next/router";
+import Layout from "@/layouts/Layout.jsx"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function RoutingComponent(props) {
+  const router = props.router;
+  if (
+    router.pathname == "/login" ||
+    router.pathname == "/reset-password" ||
+    router.pathname == "/landing-page-two" ||
+    router.pathname == "/landing-page"
+  ) {
+    // return <Fullpage className="fullPage">{props.children}</Fullpage>
+    return
+  } else {
+    return (
+      <>
+        <Layout>{props.children}</Layout>
+      </>
+    );
+  }
 }
 
-export default MyApp;
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />;
+// }
+
+// export default MyApp;
+
+export default function App({ Component, pageProps: { ...pageProps } }) {
+  const router = useRouter();
+
+  return (
+    <RoutingComponent router={router}>
+      <Component {...pageProps} />
+    </RoutingComponent>
+  );
+}
