1b90231d57041d87283979dc0714026b59ff9d3c
Md. Ajgar Hossain (Jewell) <mdajgarhossain1@gmail.com>
2023-09-30 14:31:15 +0600

    Formatted

--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -1,8 +1,8 @@
-import CategoryList from '@/components/product/CategoryList';
-import ProductList from '@/components/product/ProductList';
-import categories from '@/utils/categories';
-import demoCategories from '@/utils/demoCategories';
-import React, { useState } from 'react'
+import CategoryList from "@/components/product/CategoryList";
+import ProductList from "@/components/product/ProductList";
+import categories from "@/utils/categories";
+import demoCategories from "@/utils/demoCategories";
+import React, { useState } from "react";
 
 function Products() {
   // return (
@@ -17,23 +17,25 @@ function Products() {
     // Update the 'products' state with the fetched data
     // For now, you can use placeholder data
     const placeholderProducts = [
-      { name: 'Product 1' },
-      { name: 'Product 2' },
-      { name: 'Product 3' },
+      { name: "Product 1" },
+      { name: "Product 2" },
+      { name: "Product 3" },
     ];
     setProducts(placeholderProducts);
   };
 
   return (
-    <div className='max-w-[1200px] mx-auto mt-3 lg:pb-10'>
+    <div className="max-w-[1200px] mx-auto mt-3 lg:pb-10">
       <h2 className="headline text-[#5f27cd] py-2">Product</h2>
       <div className="container mx-auto mt-2 p-2">
-      <CategoryList categories={demoCategories} onCategoryClick={handleCategoryClick} />
-      <ProductList products={products} />
+        <CategoryList
+          categories={demoCategories}
+          onCategoryClick={handleCategoryClick}
+        />
+        <ProductList products={products} />
+      </div>
     </div>
-    </div>
-    
   );
 }
 
-export default Products
+export default Products;
