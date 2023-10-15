d12e4906cc0fd6cea5b948374c41b3f9d933731b
Md. Ajgar Hossain (Jewell) <mdajgarhossain1@gmail.com>
2023-10-15 21:52:05 +0600

    Added validation

--- src/pages/admin/products/add-product/index.jsx
+++ src/pages/admin/products/add-product/index.jsx
@@ -1,6 +1,9 @@
 import SearchSelect from "@/components/common/SearchSelect";
 import { useRouter } from "next/router";
 import { Fragment, useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 const AddProduct = () => {
   const [processing, setProcessing] = useState(false);
@@ -21,6 +24,26 @@ const AddProduct = () => {
   const [subCategoryLoading, setSubCategoryLoading] = useState(false);
   const [selectedSubCategory, setSelectedSubCategory] = useState(null);
 
+  const schema = yup.object().shape({
+    name: yup.string().required("Name is required"),
+    category: yup.string().required("Category is required"),
+    subCategory: yup.string().required("Sub Category is required"),
+    details: yup.string(),
+  });
+
+  // Form validation hooks
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    trigger,
+    reset,
+    watch,
+    formState: { errors },
+  } = useForm({
+    mode: "onChange",
+    resolver: yupResolver(schema),
+  });
 
   // useEffect(() => {
   //   getCategory();
@@ -30,14 +53,10 @@ const AddProduct = () => {
   //   getSubCategory();
   // }, [router]);
 
-  // useEffect(() => {
-  //   setValue("category", selectedCategory?.id ?? null);
-  //   setValue("subCategory", selectedSubCategory?.id ?? null);
-  
-  // }, [
-  //   selectedCategory,
-  //   selectedSubCategory,
-  // ]);
+  useEffect(() => {
+    setValue("category", selectedCategory?.id ?? null);
+    setValue("subCategory", selectedSubCategory?.id ?? null);
+  }, [selectedCategory, selectedSubCategory]);
 
   /**
    * Retrieve categories.
@@ -84,40 +103,65 @@ const AddProduct = () => {
     setSelectedCategory(data);
 
     // Revalidate the category.
-    // setTimeout(() => {
-    //   trigger("category");
-    // }, 500);
+    setTimeout(() => {
+      trigger("category");
+    }, 500);
   }
 
   /**
-   * Set category.
+   * Set sub category.
    */
   function handleSubCategory(data) {
     setSelectedSubCategory(data);
 
-    // Revalidate the category.
-    // setTimeout(() => {
-    //   trigger("category");
-    // }, 500);
+    // Revalidate the sub category.
+    setTimeout(() => {
+      trigger("subCategory");
+    }, 500);
+  }
+
+  /**
+   * Add product item.
+  */
+  function addProduct(data) {
+    console.log({data});
+    // setProcessing(true);
+    let formData = new FormData();
+    formData.append("name", data.name);
+    formData.append("categoryId", data.category);
+    formData.append("subCategoryId", data.subCategory);
+    formData.append("details", data.details);
+
+    console.log("formData", formData);
+
+    // dokaneApi
+    //   .post("/restaurant/foods", formData)
+    //   .then((response) => {
+    //     toast.success("Food is created", { duration: 3000 });
+    //     router.push("/restaurant/food");
+    //   })
+    //   .catch((error) => {
+    //     setProcessing(false);
+    //     if (error.response?.data?.type === "ValidationException") {
+    //       toast.error(error?.response?.errors[0]?.message, { duration: 3000 });
+    //     }
+    //   });
   }
 
   /**
    * Reset all data form inputs.
    */
   function resetAllValue() {
-    // setValue("category",  null);
+    setValue("category",  null);
     setSelectedCategory(null);
 
-    // setValue("subCategory",  null);
+    setValue("subCategory",  null);
     setSelectedSubCategory(null);
 
-    // reset({
-    //   title: null,
-    //   unit_cost: null,
-    //   food_code: null,
-    //   price: null,
-    //   discount: null
-    // });
+    reset({
+      name: null,
+      details: null,
+    });
   }
 
   return (
@@ -127,10 +171,15 @@ const AddProduct = () => {
           Add Product
         </h1> */}
         <form className="bg-white shadow-sm ring-1 ring-gray-900/5">
-          <h1 className="px-4 py-6 sm:py-8 sm:px-6 mt-2 text-lg font-semibold leading-6 text-gray-900">
+          <h1 className="px-4 py-2 sm:py-2 sm:px-6 mt-4 text-lg font-semibold leading-6 text-gray-900">
             Add Product
           </h1>
-          <div className="px-4 py-6 sm:py-8 sm:px-6">
+          <span className="px-4 sm:px-6">
+            The fields labels marked with
+            <span className="text-lg text-red-700"> *</span> are required input
+            field
+          </span>
+          <div className="px-4 py-6 sm:py-8 sm:px-6 mt-3">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-8">
               <div>
                 <label
@@ -141,12 +190,18 @@ const AddProduct = () => {
                 </label>
                 <div className="mt-2">
                   <input
+                    {...register("name")}
                     type="text"
                     name="name"
                     id="name"
                     autoComplete="given-name"
                     className="w-full px-4 py-2 text-base border border-gray-300 rounded focus:outline-primary focus:ring-primary focus:border-primary"
                   />
+                  {errors.name && (
+                    <p className="text-sm text-red-500">
+                      {errors.name?.message}
+                    </p>
+                  )}
                 </div>
               </div>
 
@@ -191,11 +246,11 @@ const AddProduct = () => {
                       categoryLoading ? "Fetching data" : "Select Category"
                     }
                   />
-                  {/* {errors.category && (
-                  <p className="text-sm text-red-500">
-                    {errors.category?.message}
-                  </p>
-                )} */}
+                  {errors.category && (
+                    <p className="text-sm text-red-500">
+                      {errors.category?.message}
+                    </p>
+                  )}
                 </div>
               </div>
             </div>
@@ -240,14 +295,16 @@ const AddProduct = () => {
                     isClearable={true}
                     isLoading={subCategoryLoading}
                     placeholder={
-                      subCategoryLoading ? "Fetching data" : "Select Sub Category"
+                      subCategoryLoading
+                        ? "Fetching data"
+                        : "Select Sub Category"
                     }
                   />
-                  {/* {errors.subCategory && (
-                  <p className="text-sm text-red-500">
-                    {errors.subCategory?.message}
-                  </p>
-                )} */}
+                  {errors.subCategory && (
+                    <p className="text-sm text-red-500">
+                      {errors.subCategory?.message}
+                    </p>
+                  )}
                 </div>
               </div>
 
@@ -256,26 +313,67 @@ const AddProduct = () => {
                   htmlFor="details"
                   className="block text-sm font-medium leading-6 text-gray-900"
                 >
-                  Details <span className="text-red-600">*</span>
+                  Details
                 </label>
                 <div className="mt-2">
                   <input
-                    id="category"
-                    name="category"
+                    {...register("details")}
+                    id="details"
+                    name="details"
                     type="text"
-                    autoComplete="category"
+                    autoComplete="details"
                     className="w-full px-4 py-2 text-base border border-gray-300 rounded focus:outline-primary focus:ring-primary focus:border-primary"
                   />
                 </div>
               </div>
             </div>
           </div>
-          <div className="flex flex-row-reverse items-center px-4 py-1 mb-3 sm:px-6">
+          {/* <div className="flex flex-row-reverse items-center px-4 py-1 mb-3 sm:px-6">
             <button
               type="submit"
               className="block mt-4 rounded-md bg-[#464e6e] px-6 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-[#464e6e] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Submit
+            </button>
+          </div> */}
+          <div className="flex items-center justify-end gap-x-6 px-4 py-4 sm:px-8 mt-4">
+            <button
+              type="button"
+              className="text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100 border-2 py-2 px-4 rounded"
+              onClick={resetAllValue}
+            >
+              Reset
+            </button>
+
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              disabled={processing}
+              onClick={handleSubmit(addProduct)}
+            >
+              {processing ? (
+                <div className="flex items-center">
+                  <svg
+                    aria-hidden="true"
+                    className="w-5 h-5 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
+                  Creating...
+                </div>
+              ) : (
+                "Create"
+              )}
             </button>
           </div>
         </form>
