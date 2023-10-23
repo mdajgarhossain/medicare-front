de65b9faa2c77b3c53d2bb0bb755adfdf0012109
Md. Ajgar Hossain (Jewell) <mdajgarhossain1@gmail.com>
2023-10-23 22:24:09 +0600

    Checked if authorized

--- src/utils/http/index.js
+++ src/utils/http/index.js
@@ -7,10 +7,16 @@ const medicareApi = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   //withCredentials: true,
   headers: {
-    'Authorization': `Bearer ${theCookies.get('auth_user_token')}`,
+    // 'Authorization': `Bearer ${theCookies.get('auth_user_token')}`,
     'Content-Type': 'application/json'
   },
 })
+
+// Add Authorization header if 'auth_user_token' is present in cookies
+const authUserToken = theCookies.get('auth_user_token');
+if (authUserToken) {
+  medicareApi.defaults.headers['Authorization'] = `Bearer ${authUserToken}`;
+}
 
 // Response interceptor of dokaneApi instance.
 medicareApi.interceptors.response.use(
