5a5f47583d42ea1655491512117dc69f25b8ed2f
Md. Ajgar Hossain (Jewell) <mdajgarhossain1@gmail.com>
2023-10-24 20:28:17 +0600

    Integrated api with create account

--- src/pages/create-account/index.jsx
+++ src/pages/create-account/index.jsx
@@ -1,9 +1,104 @@
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import Link from "next/link";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { medicareApi } from "@/utils/http";
+import toast from "react-hot-toast";
+import { useRouter } from "next/router";
 
 const CreateAccount = () => {
+  const [processing, setProcessing] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
-  const [error, setError] = useState();
+  const router = useRouter();
+
+  const schema = yup.object().shape({
+    name: yup.string().required("Name is required"),
+    email: yup
+      .string()
+      .email("Invalid email format")
+      .required("Email is required"),
+    password: yup
+      .string()
+      .required("Password is required")
+      .min(6, "Password must be at least 6 characters"),
+    phone: yup.string(),
+    address: yup.string(),
+    city: yup.string(),
+    state: yup.string(),
+    zipCode: yup.string(),
+  });
+
+  // Form validation hooks
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    trigger,
+    reset,
+    watch,
+    formState: { errors },
+  } = useForm({
+    mode: "onChange",
+    resolver: yupResolver(schema),
+  });
+
+  /**
+   * Create Account.
+   */
+  function createAccount(data) {
+    setProcessing(true);
+    let formData = new FormData();
+
+    // Define the list of fields you want to include in the formData
+    const fieldsToInclude = [
+      "name",
+      "email",
+      "password",
+      "phone",
+      "address",
+      "city",
+      "state",
+      "zipCode",
+    ];
+
+    // Use a loop to append valid and non-empty fields to formData
+    fieldsToInclude.forEach((field) => {
+      if (data[field]) {
+        formData.append(field, data[field]);
+      }
+    });
+
+    medicareApi
+      .post("/user/register", formData)
+      .then((response) => {
+        toast.success("Account is created", { duration: 3000 });
+        resetAllValue();
+        router.push("/sign-in");
+      })
+      .catch((error) => {
+        setProcessing(false);
+        if (error.response?.data?.type === "ValidationException") {
+          toast.error(error?.response?.errors[0]?.message, { duration: 3000 });
+        }
+      });
+  }
+
+  /**
+   * Reset all data form inputs.
+   */
+  function resetAllValue() {
+    reset({
+      name: null,
+      email: null,
+      password: null,
+      phone: null,
+      address: null,
+      city: null,
+      state: null,
+      zipCode: null,
+    });
+  }
 
   // password shaw and hide handler
   const togglePasswordVisibility = () => {
@@ -21,11 +116,15 @@ const CreateAccount = () => {
                 Name <span className="text-[red]">*</span>
               </label>
               <input
+                {...register("name")}
                 type="text"
                 id="name"
                 className={`outline-none w-full p-2 border border-gray-300 rounded-md`}
                 placeholder="Sharmin Jahan Lucky"
               />
+              {errors.name && (
+                <p className="text-sm text-red-500">{errors.name?.message}</p>
+              )}
             </div>
 
             <div className="mb-4">
@@ -33,10 +132,14 @@ const CreateAccount = () => {
                 Email <span className="text-[red]">*</span>
               </label>
               <input
+                {...register("email")}
                 type="email"
                 className={`outline-none w-full p-2 border border-gray-300 rounded-md`}
                 placeholder="jimi.uae@gmail.com"
               />
+              {errors.email && (
+                <p className="text-sm text-red-500">{errors.email?.message}</p>
+              )}
             </div>
 
             <div className="mb-4">
@@ -50,6 +153,7 @@ const CreateAccount = () => {
               /> */}
               <div className="relative">
                 <input
+                  {...register("password")}
                   type={showPassword ? "text" : "password"}
                   className={`outline-none w-full p-2 border border-gray-300 rounded-md pr-12`}
                   placeholder="Enter your password"
@@ -86,62 +190,122 @@ const CreateAccount = () => {
                   )}
                 </button>
               </div>
+              {errors.password && (
+                <p className="text-sm text-red-500">
+                  {errors.password?.message}
+                </p>
+              )}
             </div>
 
             <div className="mb-4">
-              <label className="block mb-2 font-medium">
-                Phone
-              </label>
+              <label className="block mb-2 font-medium">Phone</label>
               <input
+                {...register("phone")}
                 type="text"
                 className={`outline-none w-full p-2 border border-gray-300 rounded-md`}
                 placeholder="+880 088 8888 888"
               />
+              {errors.phone && (
+                <p className="text-sm text-red-500">{errors.phone?.message}</p>
+              )}
             </div>
 
             <div className="mb-4">
-              <label className="block mb-2 font-medium">
-                Address
-              </label>
+              <label className="block mb-2 font-medium">Address</label>
               <input
+                {...register("address")}
                 type="text"
                 className={`outline-none w-full p-2 border border-gray-300 rounded-md`}
                 placeholder="UAE, Sharjah"
               />
+              {errors.address && (
+                <p className="text-sm text-red-500">
+                  {errors.address?.message}
+                </p>
+              )}
             </div>
 
             <div className=" flex gap-3">
               <div className="mb-4 w-full ">
                 <label className="block mb-2 font-medium">City</label>
                 <input
+                  {...register("city")}
                   type="text"
                   className="w-full p-2 border border-gray-300 rounded-md"
                   placeholder="Rolla"
                 />
+                {errors.city && (
+                  <p className="text-sm text-red-500">{errors.city?.message}</p>
+                )}
               </div>
               <div className="mb-4 w-full">
                 <label className="block mb-2 font-medium">State</label>
                 <input
+                  {...register("state")}
                   type="text"
                   className="w-full p-2 border border-gray-300 rounded-md"
                   placeholder="Sharjah"
                 />
+                {errors.state && (
+                  <p className="text-sm text-red-500">
+                    {errors.state?.message}
+                  </p>
+                )}
               </div>
               <div className="mb-4 w-full">
                 <label className="block mb-2 font-medium">Zip Code</label>
                 <input
+                  {...register("zipCode")}
                   type="number"
                   className="w-full p-2 border border-gray-300 rounded-md"
                   placeholder="00000"
                 />
+                {errors.zipCode && (
+                  <p className="text-sm text-red-500">
+                    {errors.zipCode?.message}
+                  </p>
+                )}
               </div>
             </div>
-            <button
-              type="submit"
-              className={`w-full py-2 px-4 bg-blue-500 text-white rounded-md font-medium mt-4 hover:bg-blue-600`}
+            <div className="flex items-center justify-end gap-x-6 px-4 py-4 sm:px-8 mt-4">
+              {/* <button
+              type="button"
+              className="text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100 border-2 py-2 px-4 rounded"
+              onClick={resetAllValue}
             >
-              Submit
-            </button>
+              Reset
+            </button> */}
+              <button
+                onClick={handleSubmit(createAccount)}
+                disabled={processing}
+                type="submit"
+                className={`w-full py-2 px-4 bg-blue-500 text-white rounded-md font-medium mt-4 hover:bg-blue-600`}
+              >
+                {processing ? (
+                  <div className="flex items-center">
+                    <svg
+                      aria-hidden="true"
+                      className="w-5 h-5 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                      viewBox="0 0 100 101"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="currentFill"
+                      />
+                    </svg>
+                    Processing...
+                  </div>
+                ) : (
+                  "Submit"
+                )}
+              </button>
+            </div>
           </form>
           <div className="flex justify-center gap-2 mt-4">
             <p className="text-md text-center dark:text-gray-400">
