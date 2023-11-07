5904d20d53eab83504295cacca701d9e9e60236a
Md. Ajgar Hossain (Jewell) <mdajgarhossain1@gmail.com>
2023-11-08 01:39:40 +0600

    Populate dashboard total data

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,21 +1,75 @@
-import React from 'react';
-import Sidebar from './Sidebar';
-import Card from './DashboardCard';
-import { UserIcon, CubeIcon, ViewListIcon, CurrencyDollarIcon } from '@heroicons/react/solid';
+import React, { useEffect, useState } from "react";
+import Card from "./DashboardCard";
+import {
+  UserIcon,
+  CubeIcon,
+  ViewListIcon,
+  CurrencyDollarIcon,
+} from "@heroicons/react/solid";
+import { medicareApi } from "@/utils/http";
 
 const Dashboard = () => {
+  const [theDashboardData, setTheDashboardData] = useState(null);
+
+  useEffect(() => {
+    getDashboardData();
+  }, []);
+
+  function getDashboardData() {
+    medicareApi
+      .get("/dashboard")
+      .then((response) => {
+        setTheDashboardData(response.data);
+      })
+      .catch((error) => {});
+  }
+
+  // Calculate the total sales amount from the 'totalOrder' array
+  const calculateTotalSalesAmount = () => {
+    if (theDashboardData) {
+      const totalOrder = theDashboardData.find((item) => item.type === "order");
+      if (totalOrder && totalOrder.totalOrder) {
+        return totalOrder.totalOrder.reduce(
+          (total, order) => total + order.totalCost,
+          0
+        );
+      }
+    }
+    return 0;
+  };
+
   return (
-    // <div className="flex h-screen">
-    //   <Sidebar />
-      <div className="flex-1 p-8">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-          <Card title="Total Users" value="100" icon={<UserIcon className="w-6 h-6" />} />
-          <Card title="Total Products" value="250" icon={<CubeIcon className="w-6 h-6" />} />
-          <Card title="Total Orders" value="50" icon={<ViewListIcon className="w-6 h-6" />} />
-          <Card title="Total Sales Amount" value="$10,000" icon={<CurrencyDollarIcon className="w-6 h-6" />} />
-        </div>
+    <div className="flex-1 p-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <Card
+          title="Total Users"
+          value={
+            theDashboardData?.find((item) => item.type === "user")?.total || 0
+          }
+          icon={<UserIcon className="w-6 h-6" />}
+        />
+        <Card
+          title="Total Products"
+          value={
+            theDashboardData?.find((item) => item.type === "product")?.total ||
+            0
+          }
+          icon={<CubeIcon className="w-6 h-6" />}
+        />
+        <Card
+          title="Total Orders"
+          value={
+            theDashboardData?.find((item) => item.type === "order")?.total || 0
+          }
+          icon={<ViewListIcon className="w-6 h-6" />}
+        />
+        <Card
+          title="Total Sales Amount"
+          value={`$${calculateTotalSalesAmount()}`}
+          icon={<CurrencyDollarIcon className="w-6 h-6" />}
+        />
       </div>
-    // </div>
+    </div>
   );
 };
 
