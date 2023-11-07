f2836c65dc07adad1d5f6c8445a7dfa15d155932
Md. Ajgar Hossain (Jewell) <mdajgarhossain1@gmail.com>
2023-11-08 01:04:40 +0600

    Added order details

--- src/pages/admin/orders/order-details/index.jsx
+++ src/pages/admin/orders/order-details/index.jsx
@@ -18,7 +18,7 @@ const OrderDetails = () => {
       .get(`/order?include=o.orderBy,o.updatedBy,o.products&orderId=${id}`)
       .then((response) => {
         let order = response.data.data[0];
-        console.log({order});
+        console.log({ order });
         setTheOrder(order);
       })
       .catch((error) => {
@@ -35,11 +35,78 @@ const OrderDetails = () => {
   }
 
   return (
-    <div className="grid p-4 mt-2 gap-y-8">
-      <div className="bg-white shadow-sm ring-1 ring-gray-900/5">
-        <h1 className="px-4 py-2 sm:py-2 sm:px-6 my-3 text-lg font-semibold leading-6 text-gray-900">
+    <div className="p-4 mt-2">
+      <div className="bg-white shadow-md p-4 rounded-lg">
+        <h1 className="text-3xl font-semibold mb-4 text-gray-800">
           Order Details
         </h1>
+        <div className="grid grid-cols-2 gap-4 bg-gray-200 p-3">
+          <div>
+            <h2 className="text-xl font-semibold text-gray-800">
+              Order Information
+            </h2>
+            <div className="text-gray-600">
+              <p>
+                <strong>Order ID:</strong> {theOrder.id}
+              </p>
+              <p>
+                <strong>Created At:</strong> {theOrder.createdAt}
+              </p>
+              <p>
+                <strong>Shipping Address:</strong> {theOrder.shippingAddress}
+              </p>
+              <p>
+                <strong>House Address:</strong> {theOrder.billingAddress}
+              </p>
+              <p>
+                <strong>Order Status:</strong> {theOrder.orderStatus}
+              </p>
+              <p>
+                <strong>Payment Method:</strong> {theOrder.paymentMethod}
+              </p>
+              <p>
+                <strong>Payment Status:</strong> {theOrder.paymentStatus}
+              </p>
+              <p>
+                <strong>Shipping Cost:</strong> ${theOrder.shippingCost}
+              </p>
+              <p>
+                <strong>Total Cost:</strong> ${theOrder.totalCost}
+              </p>
+            </div>
+          </div>
+          <div>
+            <h2 className="text-xl font-semibold text-gray-800">Ordered By</h2>
+            <div className="text-gray-600">
+              <p>
+                <strong>Name:</strong> {theOrder?.orderBy?.name}
+              </p>
+              <p>
+                <strong>Email:</strong> {theOrder?.orderBy?.email}
+              </p>
+              <p>
+                <strong>Type:</strong> {theOrder?.orderBy?.type}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="bg-gray-200 p-3">
+          <h2 className="text-xl font-semibold text-gray-800 mt-4">
+            Ordered Products
+          </h2>
+          <div className="text-gray-600">
+            {theOrder?.products?.map((product) => (
+              <div key={product.id} className="mb-4">
+                <p>
+                  <strong>Name:</strong> {product.name}
+                </p>
+                <p>
+                  <strong>Description:</strong> {product.description}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
